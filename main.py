8f43a343e50eb0f3ddac561e8d6fdd1fbe20a3a6
Nakaguchi Takatoshi <takatoshi.n@fuji.waseda.jp>
2023-11-14 10:19:52 +0900

    update

--- main.py
+++ main.py
@@ -33,46 +33,49 @@ for c in range(C):
         w_bar[c][k] = (w[c][k] - m[c]) / (M[c] - m[c])
 # ========================================================
 
-# 変数配列の生成
-gen = SymbolGenerator(BinaryPoly)
-q = gen.array(shape=(T,C,K))
-# バイナリ多項式の構築
+def main():
+    # 変数配列の生成
+    gen = SymbolGenerator(BinaryPoly)
+    q = gen.array(shape=(T,C,K))
+    # バイナリ多項式の構築
 
-# 選んだ服の暖かさ平均
-A = [0] * T
-for t in range(T):
-    A[t] = sum_poly(C, lambda c: sum_poly(K, lambda k: w_bar[c][k] * q[t, c, k])) / C
-# 分散
-D = sum_poly(T, lambda t: sum_poly(C, lambda c: ((sum_poly(K, lambda k: w_bar[c][k] * q[t, c, k])) - A[t]) ** 2) / C)
+    # 選んだ服の暖かさ平均
+    A = [0] * T
+    for t in range(T):
+        A[t] = sum_poly(C, lambda c: sum_poly(K, lambda k: w_bar[c][k] * q[t, c, k])) / C
+    # 分散
+    D = sum_poly(T, lambda t: sum_poly(C, lambda c: ((sum_poly(K, lambda k: w_bar[c][k] * q[t, c, k])) - A[t]) ** 2) / C)
 
-# トップスとズボンはone-hot
-f1 = sum_poly(T, lambda t: (sum_poly(C-1, lambda c: (sum_poly(K, lambda k: q[t, c, k]) - 1) ** 2)))
-# アウター ≤ 1
-f2 = sum_poly(T, lambda t: (1- sum_poly(K, lambda k: q[t, 2, k]) * 2) ** 2 -1)
-# 前日と同じ服は着ない
-f3 = sum_poly(T, lambda t: (sum_poly(C, lambda c: (sum_poly(K, lambda k: q[t, c, k] * q[(t+1)%T, c, k])))))
+    # トップスとズボンはone-hot
+    f1 = sum_poly(T, lambda t: (sum_poly(C-1, lambda c: (sum_poly(K, lambda k: q[t, c, k]) - 1) ** 2)))
+    # アウター ≤ 1
+    f2 = sum_poly(T, lambda t: (1- sum_poly(K, lambda k: q[t, 2, k]) * 2) ** 2 -1)
+    # 前日と同じ服は着ない
+    f3 = sum_poly(T, lambda t: (sum_poly(C, lambda c: (sum_poly(K, lambda k: q[t, c, k] * q[(t+1)%T, c, k])))))
 
-# 目的関数の設定
-# 暖かさ
-g = sum_poly(T, lambda t: (sum_poly(C, lambda c: sum_poly(K, lambda k: w[c][k] * q[t, c, k])) - W[t]) ** 2)
+    # 目的関数の設定
+    # 暖かさ
+    g = sum_poly(T, lambda t: (sum_poly(C, lambda c: sum_poly(K, lambda k: w[c][k] * q[t, c, k])) - W[t]) ** 2)
 
-model = BinaryQuadraticModel(D+f1+f2+f3+g)
+    model = BinaryQuadraticModel(D+f1+f2+f3+g)
 
-# イジングマシンクライアントの設定
-client = FixstarsClient()
-client.token = my_token
-client.parameters.timeout = 1000  # タイムアウト1秒
+    # イジングマシンクライアントの設定
+    client = FixstarsClient()
+    client.token = my_token
+    client.parameters.timeout = 1000  # タイムアウト1秒
 
-# ソルバーの実行
-solver = Solver(client)
-result = solver.solve(model)
+    # ソルバーの実行
+    solver = Solver(client)
+    result = solver.solve(model)
 
-# 結果の解析
-for solution in result:
-    print(f'energy = {solution.energy}')
-    q_array = q.decode(solution.values)
+    # 結果の解析
+    for solution in result:
+        print(f'energy = {solution.energy}')
+        q_array = q.decode(solution.values)
+        print_array(q_array)
+        break # solutionが複数の場合も1つだけ出力
 
-    # ===================================================
+def print_array(q_array):
     # print w
     print("w    ",end="|")
     for c in range(C):
@@ -100,5 +103,6 @@ for solution in result:
                     print(" ␣  ",end=",")
             print("|",end="")
         print(f" {W[t]:2d}  {sum_w:2d}")
-    # ===================================================
-    break
+
+if __name__ == '__main__':
+    main()

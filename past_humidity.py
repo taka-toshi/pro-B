747b990c5c5d07ea7f7cedc077ff557d74860f80
kbase6 <koumatsudass06@gmail.com>
2023-12-18 14:05:58 +0900

    revised past_humidity

--- past_humidity.py
+++ past_humidity.py
@@ -1,18 +1,27 @@
 from datetime import datetime
-from collections import OrderedDict
 
 import pandas as pd
 from tabulate import tabulate
 
-dict_humidity = OrderedDict()
 year , month , day = datetime.now().strftime("%Y,%m,%d").split(",")
+day = 7
 prec_no = 44 # 東京都
 block_no = 47662 # 東京
+
+df2_humidity = pd.DataFrame()
+## obtain the humidity of last month if today is earlier than 8
+if int(day) < 8:
+  url2 = f"https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no={prec_no}&block_no={block_no}&year={year}&month={str(int(month)-1)}&day=&view=p1"
+  table2 = pd.read_html(url2)
+  df2 = table2[0]
+  # 過去1週間の湿度
+  df2_humidity = df2["湿度(％)"]
+  # headerを変える
+  headers = ["平均", "最小"]
+  df2_humidity = df2_humidity.set_axis(headers, axis="columns")
+
+## obtain the humidity of the past seven days til yesterday
 url = f"https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no={prec_no}&block_no={block_no}&year={year}&month={month}&day=&view=p1"
-
-#if int(day) < 8:
-#    url2 = f"https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no={prec_no}&block_no={block_no}&year={year}&month={month-1}&day=&view=p1"
-
 table = pd.read_html(url)
 df = table[0]
 # 過去1週間の湿度
@@ -20,11 +29,12 @@ df_humidity = df["湿度(％)"]
 # headerを変える
 headers = ["平均", "最小"]
 df_humidity = df_humidity.set_axis(headers, axis="columns")
-for i, humidity in enumerate(df_humidity["平均"]):
-    if i+1 > int(day) -1:
-        break
-    elif i+1 > int(day) -8:
-        dict_humidity[i+1] = humidity
+bottom = 0 if int(day) < 8 else (int(day) - 8)
 
-for key, val in dict_humidity.items():
-    print(key, val)
+df_combined = pd.concat([df2_humidity[int(day)-8:], df_humidity[bottom:int(day)-1]], axis=0)
+print(tabulate(df_combined, headers='keys', tablefmt='psql'))
+
+## average humidity of the past week
+sum_humidity = sum(df_combined["平均"])
+ave_humidity = sum_humidity / len(df_combined["平均"])
+print(ave_humidity)
